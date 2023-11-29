19aa063ee4b5f925583874fbcc5ceeab93b8d43b
20120574 <duytan030522@gmail.com>
2023-11-29 09:53:01 +0700

    WNC-6: add passport, active account by email, forgot and reset password

---
+++ src/middlewares/requireAuth.js
@@ -0,0 +1,30 @@
+const passport = require("passport");
+const JwtStrategy = require("passport-jwt").Strategy;
+const usersService = require("../services/users");
+
+const jwtOptions = {
+  jwtFromRequest: (req) => {
+    let token = null;
+    if (req && req.cookies) {
+      token = req.cookies.accessToken;
+    }
+    return token;
+  },
+  secretOrKey: process.env.AT_SECRET_KEY,
+};
+
+passport.use(
+  new JwtStrategy(jwtOptions, async (payload, done) => {
+    const user = await usersService.getUserById(payload.sub);
+
+    if (user) {
+      return done(null, { sub: user.id, email: user.email });
+    } else {
+      return done(null, false);
+    }
+  })
+);
+
+const requireAuth = passport.authenticate("jwt", { session: false });
+
+module.exports = requireAuth;
--- src/services/files.js
+++ src/services/files.js
@@ -4,9 +4,11 @@ const {
   GetObjectCommand,
 } = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const axios = require("axios");
+const { v4: uuidv4 } = require("uuid");
 
 const s3Client = new S3Client({
-  region: "ap-southeast-2",
+  region: process.env.S3_BUCKET_REGION,
   credentials: {
     accessKeyId: process.env.S3_ACCESS_KEY,
     secretAccessKey: process.env.S3_SECRET_KEY,
@@ -28,6 +30,26 @@ module.exports = {
     try {
       const command = new PutObjectCommand(params);
       await s3Client.send(command);
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  },
+
+  createImageByUrl: async (url) => {
+    try {
+      const response = await axios.get(url, { responseType: "arraybuffer" });
+      const fileName = `${uuidv4()}.jpg`;
+
+      const params = {
+        Bucket: process.env.S3_BUCKET_NAME,
+        Key: fileName,
+        Body: response.data,
+        ContentType: "jpg",
+      };
+      const command = new PutObjectCommand(params);
+      await s3Client.send(command);
+      return fileName;
     } catch (error) {
       console.error(error);
       return false;
